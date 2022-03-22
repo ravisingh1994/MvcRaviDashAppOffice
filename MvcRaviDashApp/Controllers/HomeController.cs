43c70dea1dbed2018250b4008362f99e3fe4f459
admin <admin@CHETUIWK1330>
2022-03-22 14:22:59 +0530

    Add project files.

---
+++ MvcRaviDashApp/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using MvcRaviDashApp.DB_Employee;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcRaviDashApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetEmpData()
+        {
+            return View();
+        }
+
+        public ActionResult EmployeeDetails()
+        {
+            ChetuIndiaEntities ent = new ChetuIndiaEntities();
+
+            var empdata = ent.EmpChetus.ToList();
+
+
+            return View(empdata);
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MvcRaviDashApp/Models/EmployeeModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcRaviDashApp.Models
+{
+    public class EmployeeModel
+    {
+
+        public int eid { get; set; }
+        public string ename { get; set; }
+        public string Eemail { get; set; }
+        public string City { get; set; }
+        public int Salary { get; set; }
+    }
+}
